c43590bd26fd568c65830e2aa7e8fe1345622493
Ntwari Joshua <ntwarijoshua@gmail.com>
2019-08-22 18:10:12 +0700

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(LiveSG());
+}
+
+class LiveSG extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
